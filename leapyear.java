2a65254a1cba367282934bceeba7d5ae276bfb70
SubikshaSakthivel <subikshas450@gmail.com>
2023-10-30 07:00:57 +0530

    Leap year

---
+++ leapyear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class leapyear {
+  public static void main(String args[])
+  { System.out.println("Enter year: ");
+    Scanner in=new Scanner(System.in);
+    int leap=in.nextInt();
+    if(leap%4==0 || (leap%100==0 & leap%400==0))
+    {
+     System.out.println("Its leap year");
+    }
+    else
+    {
+      System.out.println("Its  not leap year");
+    }
+
+  }
+  
+}
