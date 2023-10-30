4cc455b5c499760ca2eecd384e45a2b34bd7ad4e
SubikshaSakthivel <subikshas450@gmail.com>
2023-10-30 05:52:44 +0530

    comment line arguments

---
+++ comlinearg.java
@@ -0,0 +1,17 @@
+public class comlinearg {
+  public static void main(String args[])
+  {
+   for(int i=0;i<args.length;i++)
+   {
+    System.out.println(args[i]);
+   }
+  }
+  
+}
+
+//output:
+/*--javac comlinearg.java
+ --java comlinearg raja mukeah yukesh
+raja
+mukeah
+yukesh*/
