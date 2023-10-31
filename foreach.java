c536a40545b18952434634c1396afeb858d01af0
SubikshaSakthivel <subikshas450@gmail.com>
2023-10-31 06:16:21 +0530

    enhanced foreach loop

---
+++ foreach.java
@@ -0,0 +1,20 @@
+public class foreach {
+  public static void main(String args[])
+  {
+    int num[]={1,2,3,4,5};
+    for(int i :num)
+    {
+      System.out.println("The flow of numbers are:"+i);
+    }
+
+
+  }
+  
+}
+/*OUTPUT:
+The flow of numbers are:1
+The flow of numbers are:2
+The flow of numbers are:3
+The flow of numbers are:4
+The flow of numbers are:5
+ */
