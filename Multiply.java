76f29fb6628c620aefabc471dc06a243ff32a4bf
SubikshaSakthivel <subikshas450@gmail.com>
2023-11-14 20:08:37 +0530

    Multiply table

--- Multiply.java
+++ Multiply.java
@@ -6,7 +6,9 @@ public class Multiply {
     Scanner in=new Scanner(System.in);
     System.out.print("Enter the number:");
     int num=in.nextInt();
-    for(int i=1;i<=num;i++)
+    System.out.print("Enter the number limit:");
+    int num1=in.nextInt();
+    for(int i=1;i<=num1;i++)
     {
       System.out.println(num +"*" +i+"="+num*i);
     }
