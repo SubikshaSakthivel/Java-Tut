8b3864a0bfa848dcddb881941e2f433e2be8b55c
SubikshaSakthivel <subikshas450@gmail.com>
2023-11-15 05:14:53 +0530

    Check String Is Palindrome

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Fibonacci 
 {
   public static void main(String args[])
   {
-    int 
+    int n1=0;
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    for(int i=0;i<=n;i++) 
+    {
+      n1=append
+      
+    }
+    System.out.println(n1);
   }
 
   
