1fb4efbe0b15e10f732ab2971bb849015f6db6fe
SubikshaSakthivel <subikshas450@gmail.com>
2023-11-15 19:22:37 +0530

    Sum Of array Elements

---
+++ SumOfArray.java
@@ -0,0 +1,22 @@
+public class SumOfArray {
+    public static void main(String[] args) 
+    {   
+        int sum=0;
+        int mark[]={2,3,4,5};
+        for(int i=0;i<=mark.length-1;i++)
+        {
+            int m=mark[i];
+            sum+=m;
+           // System.out.print(mark[i]);
+        }
+            System.out.print("Sum Of arrays:");
+            System.out.print(sum);
+        
+        
+    }
+}
+/*output
+Sum Of arrays:14
+ */
+  
+
