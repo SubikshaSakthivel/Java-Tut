f2607c71a79328fccb6af3e10ce5829f6bed5e52
SubikshaSakthivel <subikshas450@gmail.com>
2023-11-15 18:44:23 +0530

    Swaping Of Numbers

--- swap.java
+++ swap.java
@@ -2,8 +2,8 @@ class swap
 {
   public static void main(String args[])
   {
-    System.out.println("before swapping");
-    System.out.println(" a=5,b=10");
+    System.out.println("Before swapping");
+    System.out.println("a=5,b=10");
     int a=5;
     int b=10;
     a=a+b;
@@ -13,6 +13,13 @@ class swap
     System.out.println("a=" +a);
     System.out.println("b="+b);
     //the values are swapped
+    /*output
+    Before swapping
+    a=5,b=10
+    After swapping
+    a=10
+    b=5
+     */
         
 
 
