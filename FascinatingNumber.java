4adf9c13bbfad5e9e72d4d601c959463ea7001a5
SubikshaSakthivel <subikshas450@gmail.com>
2024-01-29 21:27:41 +0530

    Just Refer it

--- FascinatingNumber.java
+++ FascinatingNumber.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class FascinatingNumber{
-  public static void main(String args[])
-  {
-    int num1,n2,n3;
-    Scanner in= new Scanner(System.in);
-    System.out.println("Enter any number: ");
-    num1=in.nextInt();
-    n2=num1*2;
-    n3=num1*3;
-    String concatstr=num1+""+n2+n3;
-    boolean found=true;
-    for(int i=0;i<)
+// public class FascinatingNumber{
+//   public static void main(String args[])
+//   {
+//     int num1,n2,n3;
+//     Scanner in= new Scanner(System.in);
+//     System.out.println("Enter any number: ");
+//     num1=in.nextInt();
+//     n2=num1*2;
+//     n3=num1*3;
+//     String concatstr=num1+""+n2+n3;
+//     boolean found=true;
+//     for(int i=0;i<)
 
 
 
---
+++ README.txt
@@ -0,0 +1,3 @@
+If we want to compile then :
+Step 1:javac filename/classname.javac
+Step 2:java classname/filename
