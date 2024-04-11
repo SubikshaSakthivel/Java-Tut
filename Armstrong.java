514815562f0ad4fe6b3543db7ed7a6f3cd6ecc31
SubikshaSakthivel <subikshas450@gmail.com>
2024-04-11 21:30:00 +0530

    Armstrong Number

---
+++ Armstrong.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Armstrong  {
+    static boolean isArmstrong(int num)
+    {
+      int number=num;
+      int sum=0,temp;
+      int len=String.valueOf(num).length();
+      while(number!=0)
+      {
+        temp=number%10;
+        sum+=Math.pow(temp,len);
+        number/=10;
+
+      }
+   return sum==num;
+
+    }
+public static void main(String[] args) 
+  {
+  System.out.println("Armstrong number");
+  Scanner sum=new Scanner(System.in);
+  int num=sum.nextInt();
+ 
+if(isArmstrong(num))
+{
+  System.out.println(num+  "  is Armstrong number");
+}
+else{
+  System.out.println(num +  " is not an Armstrong number");
+}
+     
+  }
+}
+//output
+// Armstrong number
+// 8208
+// 8208  is Armstrong number
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,33 +1,75 @@
+// import java.util.Scanner;
+
+// public class PrimeNumber 
+// {
+//   public static void main(String args[])
+//   {
+//     Scanner in=new Scanner(System.in);
+//     System.out.print("Enter the number:");
+//     int n=in.nextInt();
+//     boolean isprime=true;
+//     for(int i=2;i<=n/2;i++)
+//     {
+//       if(n%i==0)
+//       {
+//          isprime=false;
+//          break;
+//       }
+//     }
+//     if(isprime)
+//     {
+//       System.out.print("prime");   
+//     }
+//     else
+//     {
+//       System.out.print("Not prime");  
+//     }
+    
+//   }
+// }
+// /*output
+//   Enter the number:21
+//   Not prime
+//  
+
 import java.util.Scanner;
 
-public class PrimeNumber 
+
+
+
+
+public class PrimeNumber
 {
-  public static void main(String args[])
+
+  static boolean isPrime(int n)
   {
-    Scanner in=new Scanner(System.in);
-    System.out.print("Enter the number:");
-    int n=in.nextInt();
-    boolean isprime=true;
-    for(int i=2;i<=n/2;i++)
-    {
-      if(n%i==0)
-      {
-         isprime=false;
-         break;
-      }
-    }
-    if(isprime)
-    {
-      System.out.print("prime");   
-    }
-    else
-    {
-      System.out.print("Not prime");  
-    }
-    
+   if(n==1)
+   {
+    return false;
+   }
+  if(n==2 || n==3)
+  {
+    return true;
+  }
+if(n%2==0 || n%3==0)
+{
+  return false;
+}
+for(int i=5;i*i<=n;i+=6)
+{
+  if(n%i==0  || n%(i+2)==0)
+  {
+    return false;
   }
 }
-/*output
-  Enter the number:21
-  Not prime
- */ 
+return true;
+  }
+  public static void main(String[] args) {
+    Scanner scan =new Scanner(System.in);
+    int n=scan.nextInt();
+    System.out.println(isPrime(n));
+
+    
+  }
+
+}
