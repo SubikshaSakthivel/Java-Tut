fdbd0e2ca17f2a5a929a3c484b433618c045c043
SubikshaSakthivel <subikshas450@gmail.com>
2024-04-16 22:19:52 +0530

    End with a Word 'e'

---
+++ EndWE.java
@@ -0,0 +1,38 @@
+public class EndWE 
+{
+       public static void main(String[] args)
+        { 
+            int start=0,i,j;
+            String ss="git provide a good platform to  enable to push and give user experience";
+            for(i=0;i<ss.length();i++)
+            {
+                if (ss.charAt(i)==' ')
+                 {
+                    start=i+1;
+                }
+               if(ss.charAt(i)=='e')
+               {
+            
+                 if(i+1==ss.length() || ss.charAt(i+1)==' ')
+                {
+                    StringBuilder sb=new StringBuilder();
+                    for(j=start;j<=i;j++)
+                    {
+                        sb.append(ss.charAt(j));
+                           
+                    }
+                    System.out.println(sb);
+
+                 }
+               
+                }
+               
+            }
+        }
+    }
+
+    //output
+    // provide
+    // enable
+    // give
+    // experience
