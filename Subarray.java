b78e73386822d3b85df39c8d29e0784635dae6b5
SubikshaSakthivel <subikshas450@gmail.com>
2024-07-16 19:16:02 +0530

    SUB ARRAY

---
+++ Subarray.java
@@ -0,0 +1,34 @@
+class Subarray 
+{
+    public static void main(String[] args) 
+    {
+        int arr[]={1,2,3,4};
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                for(int k=i;k<=j;k++)
+                {
+                System.out.print(arr[k]+" ");
+            }
+            System.out.println();
+            }
+            
+        }
+        
+        
+    }
+}
+/* output
+ * 1 
+1 2 
+1 2 3 
+1 2 3 4 
+2 
+2 3 
+2 3 4 
+3
+3 4
+4
+ */
